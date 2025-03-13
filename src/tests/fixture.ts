499059895a4a9bdf9180175da7d71b71c4b882a2
Joe Lencioni <joe.lencioni@gmail.com>
2025-03-13 11:11:41 -0500

    Fix fixture types for playwright update
    We need to tell the generic the type for this double function.

--- src/tests/fixture.ts
+++ src/tests/fixture.ts
@@ -3,7 +3,7 @@ import { test as base, mergeTests } from '@playwright/test';
 import { test as happoTest } from '..';
 
 // Empty fixture to allow importing the fixture in other files
-const baseTest = base.extend({
+const baseTest = base.extend<{ double: (number: number) => number }>({
   double: async ({}, use) => {
     await use((number: number) => number * 2);
   },
